0cbd90e48fff0475fd680cd0df5382d3dad2e37e
Apoorva Shah <apoorvashah28@gmail.com>
2017-05-28 15:39:28 +0530

    creating rooms

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ app.get('/', function(req, res){
 });
 
 //Whenever someone connects this gets executed
-var clients = 0;
+var roomno = 1;
+io.on('connection', function(socket){
 
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket){
-    console.log('someone connected');
-    nsp.emit('hi', 'Hello everyone!');
+    //Increase roomno 2 clients are present in a room.
+    if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1)
+        roomno++;
+
+
+    socket.join("room-"+roomno);
+
+    //Send this event to everyone in the room.
+    io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
 });
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
